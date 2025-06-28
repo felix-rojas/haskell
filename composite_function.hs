7160d27b4ae4bb98b4b62a74d05d0738e00e002b
felix-rojas <a01201946@tec.mx>
2025-06-27 19:33:35 -0600

    format files

--- composite_function.hs
+++ composite_function.hs
@@ -1,9 +1,13 @@
 -- All these functions should output the same result
 -- this is a composite function (cos x)²
 
+-- define square just in case
+square :: Double -> Double
+square x = x ** 2
+
 -- Literally square the cos of x
 cosSq :: Double -> Double
-cosSq x = square(cos x)
+cosSq x = square (cos x)
 
 -- Application operator takes the output from the right function
 -- and takes it to the left functionh
@@ -13,9 +17,16 @@ cosSq' x = square $ cos x
 -- This is the literal representation of a composite function
 -- like (g°f) x
 cosSq'' :: Double -> Double
-cosSq'' x = (square . cos) x
+cosSq'' = square . cos
 
 -- here it is defined as a composite function
--- it will work as long as the input type 
+-- it will work as long as the input type
 cosSq''' :: Double -> Double
 cosSq''' = square . cos
+
+-- All of these evaluate to the exact same values
+comp = do
+  cosSq (pi / 2) == cosSq' (pi / 2)
+  && (cosSq'' (pi / 2) == cosSq' (pi / 2))
+  && (cosSq'' (pi / 2) == cosSq''' (pi / 2))
+  && (cosSq (pi / 2) == cosSq''' (pi / 2))
--- functions.hs
+++ functions.hs
@@ -10,12 +10,12 @@ f x = x**2 - 3*x + 2
 h y = y**2 - 3*y + 2
 
 -- or we could define it as lambda and not declare x
-g = \x -> x**2 - 3*x + 2
+g x = x**2 - 3*x + 2
 fn = do
     -- we can define this polynomial like this
     -- to prove that y is declared, i defined h here
     -- then run it
-    putStrLn ("Value of x" ++ show (h 2)) 
-    putStrLn ("Output of f(2): " ++ show (f 2)) 
-    putStrLn ("Output of g(2): " ++ show (g 2)) 
-    putStrLn ("Is the output for both functions the same? " ++ show  ((g 2) == (f 2)))
+    putStrLn ("Value of x" ++ show (h 2))
+    putStrLn ("Output of f(2): " ++ show (f 2))
+    putStrLn ("Output of g(2): " ++ show (g 2))
+    putStrLn ("Is the output for both functions the same? " ++ show  (g 2 == f 2))
